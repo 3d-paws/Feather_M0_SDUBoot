6c98427eb021fd71803d9f9745021f2bcd79dc20
RJB <mmagithub@comet.ucar.edu>
2025-10-08 09:23:14 -0600

    20251008

---
+++ boot/sdboot.h
@@ -0,0 +1,27 @@
+/*
+ * ======================================================================================================================
+ * sdboot.h - Add SDBoot Support for UPDATE.BIN on SD card
+ * ======================================================================================================================
+ */
+
+/*
+ * ======================================================================================================================
+ * No Change Needed ~/Library/Arduino15/packages/adafruit/hardware/samd/1.7.16/variants/feather_m0/linker_scripts/gcc
+ *
+ * Replace Files in ~/Library/Arduino15/packages/adafruit/hardware/samd/1.7.16/libraries/SDU/extras/SDUBoot with
+ *                  ~/Documents/Arduino/3D-PAWS/3D-PAWS-Feather-LoRa-Remote/SDUBoot/
+ *
+ * Change directory to ~/Library/Arduino15/packages/adafruit/hardware/samd/1.7.16/libraries/SDU/extras/SDUBoot
+ * Then run ./build.sh
+ *
+ * Copy ~/Library/Arduino15/packages/adafruit/hardware/samd/1.7.16/libraries/SDU/src/boot/featherm0.h to
+ *      ~/Documents/Arduino/3D-PAWS/3D-PAWS-Feather-LoRa-Remote/FeatherLoRaRemote/boot/
+ *
+ * Include this file at top of your code and compile
+ * ======================================================================================================================
+ */
+
+__attribute__ ((section(".sketch_boot")))
+unsigned char sduBoot[0x6000] = {
+    #include "featherm0.h"
+};
